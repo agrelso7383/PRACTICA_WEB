c53ef35b9fbad2b51975ba17252424ba67d81681
agrelso7383 <s.agrela.2020@alumnos.urjc.es>
2025-04-06 19:18:43 +0200

    Update LoanService.java

--- biblioteca/biblioteca/src/main/java/com/biblioteca/biblioteca/service/LoanService.java
+++ biblioteca/biblioteca/src/main/java/com/biblioteca/biblioteca/service/LoanService.java
@@ -11,20 +11,21 @@ public class LoanService {
     private final Map<String, Loan> loanMap = new HashMap<>();
     private static int nextId = 1;
 
+    // new loan with a generated ID
     public void createLoan(Loan loan) {
         String generatedId = String.valueOf(nextId++);
         loan.setId(generatedId);
         loanMap.put(generatedId, loan);
     }
-
+// returns a loan by the ID
     public Loan getLoan(String id) {
         return loanMap.get(id);
     }
-
+// replaces a loan completely
     public void updateLoan(String id, Loan loan) {
         loanMap.put(id, loan); 
     }
-
+// updates only fields provided
     public void patchLoan(String id, Loan updatedFields) {
         Loan existingLoan = loanMap.get(id);
         if (existingLoan != null) {
@@ -43,15 +44,15 @@ public class LoanService {
             loanMap.put(id, existingLoan);
         }
     }
-
+// deletes a loan by ID
     public void deleteLoan(String id) {
         loanMap.remove(id);
     }
-
+// deletes all loans
     public void deleteAllLoans() {
         loanMap.clear();
     }
-
+// returns all loans
     public Map<String, Loan> getAllLoans() {
         return loanMap;
     }
