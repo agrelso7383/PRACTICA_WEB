f38c3c9a76669e2c637e70a482b451420a9b4d08
agrelso7383 <s.agrela.2020@alumnos.urjc.es>
2025-04-06 19:09:00 +0200

    Update WebController.java

--- biblioteca/biblioteca/src/main/java/com/biblioteca/biblioteca/controller/WebController.java
+++ biblioteca/biblioteca/src/main/java/com/biblioteca/biblioteca/controller/WebController.java
@@ -3,47 +3,48 @@ package com.biblioteca.biblioteca.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
+// Handles web page navigation
 @Controller
 public class WebController {
 
-   
+   // Loads the home page
     @GetMapping("/")
     public String home() {
         return "home";  // home.html
     }
 
-   
+   // Loads the book list page
     @GetMapping("/bookList")
     public String books() {
         return "ListBooks";  // ListBooks.html
     }
 
-
+// Loads the page for managing books (manageBooks.html)
     @GetMapping("/manageBooks")
     public String manageBooks() {
         return "manageBooks";  // manageBooks.html
     }
 
-  
+  // Loads the page for managing loans (manageLoans.html)
     @GetMapping("/manageLoans")
     public String manageLoans() {
         return "manageLoans";  // manageLoans.html
     }
 
-
+// Loads the page for managing users (manageUsers.html)
+    @GetMapping("/manageUsers")
     @GetMapping("/manageUsers")
     public String manageUsers() {
         return "manageUsers";  // manageUsers.html
     }
 
-   
+    // Loads the info page (info.html)
     @GetMapping("/info")
     public String info() {
         return "info";  // info.html
     }
 
- 
+   // Loads the About Us page (aboutUs.html)
     @GetMapping("/aboutUs")
     public String aboutUs() {
         return "aboutUs";  // aboutUs.html
