80f7ba76eb4c643542d1b5336a2a859f74352e9f
agrelso7383 <s.agrela.2020@alumnos.urjc.es>
2025-04-06 19:20:57 +0200

    Update UserService.java

--- biblioteca/biblioteca/src/main/java/com/biblioteca/biblioteca/service/UserService.java
+++ biblioteca/biblioteca/src/main/java/com/biblioteca/biblioteca/service/UserService.java
@@ -11,21 +11,24 @@ public class UserService {
     private final Map<String, User> userMap = new HashMap<>();
     private static int nextId = 1;
 
+    // adds a new user and generates an ID
     public void createUser(User user) {
         String generatedId = String.valueOf(nextId++);
         user.setId(generatedId);
         userMap.put(generatedId, user);
     }
 
+// returns a user by ID
     public User getUser(String id) {
         return userMap.get(id);
     }
-
+// replaces an user
     public void replaceUser(String id, User newUser) {
         newUser.setId(id);
         userMap.put(id, newUser);
     }
 
+    // updates the fields provided
     public void patchUser(String id, User partialUser) {
         User existingUser = userMap.get(id);
         if (existingUser != null) {
@@ -38,14 +41,15 @@ public class UserService {
         }
     }
 
+    // deletes an user by ID
     public void deleteUser(String id) {
         userMap.remove(id);
-    }
-
+    }    
+    // deletes all users
     public void deleteAllUsers() {
         userMap.clear();
     }
-
+    // returns all users
     public Map<String, User> getAllUsers() {
         return userMap;
     }
