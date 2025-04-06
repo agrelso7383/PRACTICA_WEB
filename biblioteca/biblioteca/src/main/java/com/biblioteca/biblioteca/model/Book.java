32c16864720915a3dcd9c1d6be3282801da837bd
agrelso7383 <s.agrela.2020@alumnos.urjc.es>
2025-04-06 19:10:34 +0200

    Update Book.java

--- biblioteca/biblioteca/src/main/java/com/biblioteca/biblioteca/model/Book.java
+++ biblioteca/biblioteca/src/main/java/com/biblioteca/biblioteca/model/Book.java
@@ -1,5 +1,6 @@
 package com.biblioteca.biblioteca.model;
 
+// This class represents a book with its basic information
 public class Book {
     private String id;
     private String title;
@@ -8,14 +9,14 @@ public class Book {
     // Constructor
     public Book() {}
 
-    // Constructor with parameters
+    // Constructor with parameters to create a book
     public Book(String id, String title, String author) {
         this.id = id;
         this.title = title;
         this.author = author;
     }
 
-    // Getters and Setters
+    // Getters and Setters (book ID)
     public String getId() {
         return id;
     }
@@ -23,7 +24,7 @@ public class Book {
     public void setId(String id) {
         this.id = id;
     }
-
+// book title
     public String getTitle() {
         return title;
     }
@@ -31,7 +32,7 @@ public class Book {
     public void setTitle(String title) {
         this.title = title;
     }
-
+//book author
     public String getAuthor() {
         return author;
     }
