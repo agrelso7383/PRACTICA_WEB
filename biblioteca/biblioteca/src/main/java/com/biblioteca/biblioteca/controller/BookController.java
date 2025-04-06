6cb1ee637f1c92d88897be61703fcaa0b6e1f488
agrelso7383 <s.agrela.2020@alumnos.urjc.es>
2025-04-06 18:58:54 +0200

    Update BookController.java

--- biblioteca/biblioteca/src/main/java/com/biblioteca/biblioteca/controller/BookController.java
+++ biblioteca/biblioteca/src/main/java/com/biblioteca/biblioteca/controller/BookController.java
@@ -16,37 +16,39 @@ public class BookController {
     @Autowired
     private BookService bookService;
 
+
+// Creates books from the list sent
     @PostMapping 
     public void createBooks(@RequestBody List<Book> books) {
     for (Book book : books) 
         bookService.createBook(book);
     }
-
+// Returns a book using its ID
     @GetMapping("/{id}")
     public Book getBook(@PathVariable String id) {
         return bookService.getBook(id);
     }
-
+// Returns all books with the ID as the key
     @GetMapping
     public Map<String, Book> getAllBooks() {
         return bookService.getAllBooks();
     }
-
+// Replaces a book completely
     @PutMapping("/{id}")
     public void updateBook(@PathVariable String id, @RequestBody Book book) {
         bookService.replaceBook(id, book); 
     }
-
+// Replaces only some fields of the book
      @PatchMapping("/{id}")
     public void patchBook(@PathVariable String id, @RequestBody Book book) {
         bookService.patchBook(id, book); 
     }
-
+// Deletes a book by the ID
     @DeleteMapping("/{id}")
     public void deleteBook(@PathVariable String id) {
         bookService.deleteBook(id);
     }
-
+// Deletes all books
     @DeleteMapping("/deleteAll")
     public void deleteAllBooks() {
         bookService.deleteAllBooks();
