78f0202aed6a2e0e8098269bd615ea9585306e5f
agrelso7383 <s.agrela.2020@alumnos.urjc.es>
2025-04-06 19:01:39 +0200

    Update CustomErrorController.java

--- biblioteca/biblioteca/src/main/java/com/biblioteca/biblioteca/controller/CustomErrorController.java
+++ biblioteca/biblioteca/src/main/java/com/biblioteca/biblioteca/controller/CustomErrorController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class CustomErrorController implements ErrorController {
 
+    // Handles errors and shows the error page
     @GetMapping("/error")
     public String handleError() {
         return "error"; // Muestra el template error.html
