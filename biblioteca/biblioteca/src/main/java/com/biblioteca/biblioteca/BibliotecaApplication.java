fb125a6dbf63f19df0105be7a03a01e35e27160e
agrelso7383 <s.agrela.2020@alumnos.urjc.es>
2025-04-06 19:21:31 +0200

    Update BibliotecaApplication.java

--- biblioteca/biblioteca/src/main/java/com/biblioteca/biblioteca/BibliotecaApplication.java
+++ biblioteca/biblioteca/src/main/java/com/biblioteca/biblioteca/BibliotecaApplication.java
@@ -3,6 +3,7 @@ package com.biblioteca.biblioteca;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// This is the main class that starts the Spring Boot application
 @SpringBootApplication
 public class BibliotecaApplication {
 
