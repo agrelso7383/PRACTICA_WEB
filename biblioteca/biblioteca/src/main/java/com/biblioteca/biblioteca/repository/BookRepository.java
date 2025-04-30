1543aeed497233c1d2ee906ab1c2ef42a86031c1
agrelso7383 <s.agrela.2020@alumnos.urjc.es>
2025-04-30 14:22:18 +0200

    Add files via upload

---
+++ biblioteca/biblioteca/src/main/java/com/biblioteca/biblioteca/repository/BookRepository.java
@@ -0,0 +1,10 @@
+package com.biblioteca.biblioteca.repository;
+
+import com.biblioteca.biblioteca.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
+
